76ff4b7923db88f605d8e88deadb97f25f7cf4c8
ice <shuiyunqing@outlook.com>
2018-07-09 22:02:43 +0800

    add lincense for each source file

--- src/main/java/me/ice/crypto/des/impl/DESImpl.java
+++ src/main/java/me/ice/crypto/des/impl/DESImpl.java
@@ -1,3 +1,17 @@
+/*
+ * Copyright (C) 2018 Ice
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 package me.ice.crypto.des.impl;
 
 import me.ice.crypto.des.utils.Constants;
@@ -6,7 +20,7 @@ import me.ice.crypto.des.utils.NumberUtils;
 /**
  * java 实现 DES 加密
  *
- * @author shuiyunqing
+ * @author ice
  */
 public class DESImpl {
 
